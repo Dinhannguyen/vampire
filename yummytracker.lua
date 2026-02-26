ae0f137b9373f310f377fe7718fa86810fe60eb6
Dinhannguyen <nguyenquocduynhanaov@gmail.com>
2026-02-26 11:52:29 +0700

    Add files via upload

---
+++ yummytracker.lua
@@ -0,0 +1,2 @@
+_G.Config = { UserID = "21b6852d-6359-41a2-8277-1a07f8afcb5f", discord_id = "1044791293806645361", Note = "ssss" }
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/yummytrack/main/tracker", true))()
